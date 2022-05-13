ff2033b508f401cb4b1e38604d0e072ccce35c40
marnixah <marnix@hagehoeve.com>
2022-05-13 17:11:02 +0200

    Revert "dev: more debugging"
    This reverts commit ff31a7d13dd2690d7a92fb10436106b05a0dbce6.

--- src/commands/Restart.ts
+++ src/commands/Restart.ts
@@ -1,4 +1,3 @@
-import { HttpError } from "@kubernetes/client-node";
 import Command from "eris-boreas/lib/src/conversation/Command";
 import Conversation from "eris-boreas/lib/src/conversation/Conversation";
 import Longhorn from "../lib/longhorn";
@@ -13,17 +12,8 @@ export default class Fix implements Command {
       throw new Error("You must specify a namespace");
     }
     const namespace = args[1];
-    try {
-      const longhorn = new Longhorn();
-      await longhorn.restartDeploymentsInNamespace(namespace);
-    } catch (e) {
-      console.log(JSON.stringify(e));
-      if (e instanceof HttpError) {
-        console.log(
-          `Received ${e.response.statusCode}: ${e.response.statusMessage}`
-        );
-      }
-    }
+    const longhorn = new Longhorn();
+    await longhorn.restartDeploymentsInNamespace(namespace);
     return "It should be restarted now";
   }
 }
